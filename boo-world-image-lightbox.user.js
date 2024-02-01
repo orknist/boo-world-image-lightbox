a5c1a4a98409709d1966c6fd5ed5bb98d9306cea
Orkun <orkun@email.com>
2024-02-02 01:29:05 +0300

    Update boo-world-image-lightbox.user.js

--- boo-world-image-lightbox.user.js
+++ boo-world-image-lightbox.user.js
@@ -5,6 +5,7 @@
 // @description  boo app image lightboox modal window
 // @author       orknist
 // @match        https://boo.world/*/match
+// @match        https://boo.world/*/soul/*
 // @grant        GM_addStyle
 // ==/UserScript==
 
@@ -12,39 +13,46 @@
     console.log('Boo.World Image Lightbox Loaded');
     'use strict';
 
-    var featherlightScript = document.createElement('script');
+    const featherlightScript = document.createElement('script');
     featherlightScript.src = '//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.js';
     document.head.appendChild(featherlightScript);
 
-    var featherlightStyles = document.createElement('link');
+    const featherlightStyles = document.createElement('link');
     featherlightStyles.rel = 'stylesheet';
     featherlightStyles.href = 'https://cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.css';
     document.head.appendChild(featherlightStyles);
 
-    var customStyles = `
+    const customStyles = `
         .featherlight .featherlight-content {
             padding: 0px;
         }
     `;
     GM_addStyle(customStyles);
 
-    var targetSelector = 'div.user-picture';
+    const targetSelector = 'div.user-picture';
+    function setfeatherLight($currentTargetElement) {
+        var backgroundImage = $currentTargetElement.css('background-image');
+        var backgroundImageUrl = backgroundImage.match(/url\(['"]?(.*?)['"]?\)/);
+        if (backgroundImageUrl && backgroundImageUrl[1]) {
+            var imageUrl = backgroundImageUrl[1];
+            $currentTargetElement.attr('data-featherlight', imageUrl);
+        }
+    }
+
     var observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
             mutation.addedNodes.forEach(function(node) {
                 var $targetElements = $(node).find(targetSelector);
                 $targetElements.each(function(index, element) {
-                    var $currentTargetElement = $(this);
-                    var backgroundImage = $currentTargetElement.css('background-image');
-                    var backgroundImageUrl = backgroundImage.match(/url\(['"]?(.*?)['"]?\)/);
-                    if (backgroundImageUrl && backgroundImageUrl[1]) {
-                        var imageUrl = backgroundImageUrl[1];
-                        $currentTargetElement.attr('data-featherlight', imageUrl);
-                    }
+                    setfeatherLight($(this));
                 });
             });
         });
     });
     observer.observe(document.body, { childList: true, subtree: true });
+
+    $(targetSelector).each(function(index, element) {
+        setfeatherLight($(this));
+    });
 
 })();
